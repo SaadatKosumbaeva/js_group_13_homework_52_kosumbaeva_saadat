17529da51f3d85eea07e0164d87921f13a997239
Saadat Kosumbaeva <kosumbaevasaadat@gmail.com>
2021-11-01 19:34:06 +0600

    Created simple game logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,46 @@ import {Card, CardDeck} from "./lib/CardDeck";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'homework52-app';
   cardsArray: Card[] = [];
+  gameStatus: string = '';
+  points: number = 0;
+  disabled = false;
   cardDeck;
 
   constructor() {
     this.cardDeck = new CardDeck();
     this.cardsArray = this.cardDeck.getCards(2);
-    console.log(this.cardDeck);
-    console.log(this.cardsArray);
+    this.countPoints();
+  }
+
+  countPoints() {
+    this.points = 0;
+    for (let i = 0; i < this.cardsArray.length; i++) {
+      this.points += this.cardsArray[i].getScore();
+    }
+    if (this.points === 21) {
+      this.gameStatus = 'You win!';
+      this.disabled = true;
+    } else if (this.points > 21) {
+      this.gameStatus = 'You lose!';
+      this.disabled = true;
+    }
+  }
+
+  giveAnotherCard() {
+    let oneMoreCard = this.cardDeck.getCards(1);
+    this.cardsArray.concat(oneMoreCard);
+    this.countPoints();
+  }
+
+  reset() {
+    this.disabled = false;
+    this.gameStatus = '';
+    this.cardDeck = new CardDeck();
+    this.cardsArray = this.cardDeck.getCards(2);
+    this.countPoints();
   }
 }
--- src/app/lib/CardDeck.ts
+++ src/app/lib/CardDeck.ts
@@ -1,6 +1,7 @@
 export class Card {
   rank;
   suit;
+
   constructor(rank: string, suit: string) {
     this.rank = rank;
     this.suit = suit;
@@ -16,35 +17,37 @@ export class Card {
         return 11;
       default:
         return parseInt(this.rank);
-
     }
   }
 }
 
-export class CardDeck{
-  cards: Card[] = [];
+export class CardDeck {
+  cardDeck: Card[] = [];
+  getCardsArray: Card[] = [];
+
   RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   SUITS = ['diams', 'hearts', 'clubs', 'spades'];
+
   constructor() {
     for (let i = 0; i < 13; i++) {
       for (let j = 0; j < 4; j++) {
-        this.cards.push(new Card(this.RANKS[i], this.SUITS[j]));
+        this.cardDeck.push(new Card(this.RANKS[i], this.SUITS[j]));
       }
     }
   }
 
   getCard(): Card {
-    const cardIndex = Math.floor(Math.random() * 51);
-    this.cards.splice(cardIndex, 1);
-    return this.cards[cardIndex];
+    const cardIndex = Math.floor(Math.random() * this.cardDeck.length);
+    const generatedCard = this.cardDeck[cardIndex];
+    this.cardDeck.splice(cardIndex, 1);
+    return generatedCard;
   }
 
   getCards(howMany: number): Card[] {
-    const cardsArray: Card[] = [];
     for (let i = 0; i < howMany; i++) {
-      const item = this.getCard();
-      cardsArray.push(item);
+      const currentCard = this.getCard();
+      this.getCardsArray.push(currentCard);
     }
-    return cardsArray;
+    return this.getCardsArray;
   }
 }
